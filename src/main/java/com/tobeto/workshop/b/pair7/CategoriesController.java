6e0f3dac7716e5eac2d8dd4be4615d90be7abbad
Deniz Kırıtoğlu <deniz.krtgl@gmail.com>
2023-11-01 17:15:25 +0300

    pair çalışması

---
+++ src/main/java/com/tobeto/workshop/b/pair7/CategoriesController.java
@@ -0,0 +1,28 @@
+package com.tobeto.workshop.b.pair7;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/categories")
+public class CategoriesController {
+    @GetMapping
+    public String getCategories(){
+        return "Kategoriler getirildi.";
+    }
+    @GetMapping("id")
+    public String getCategoriesById(){
+        return "Kategoriler id ye göre getirildi.";
+    }
+    @PostMapping
+    public String postCategories(){
+        return "Kategoriler eklendi.";
+    }
+    @PutMapping
+    public String putCategories(){
+        return "Kategoriler güncellendi.";
+    }
+    @DeleteMapping
+    public String deleteCategories(){
+        return "Kategoriler silindi.";
+    }
+}
